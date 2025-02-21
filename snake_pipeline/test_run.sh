b254a5beb31fac802021eeee1787a1fd00893be6
Ray <851836818@qq.com>
2025-02-21 16:35:26 -0500

    Update test_run.sh

--- snake_pipeline/test_run.sh
+++ snake_pipeline/test_run.sh
@@ -1,2 +1,2 @@
-#python accusnv_snakemake.py -i samples_cae_test_pe.csv  -r reference_genomes -o cae_pe_test
+python accusnv_snakemake.py -i samples_cae_test_pe.csv  -r reference_genomes -o cae_pe_test
 python accusnv_snakemake.py -i samples_cae_test_se.csv  -r reference_genomes -o cae_se_test
