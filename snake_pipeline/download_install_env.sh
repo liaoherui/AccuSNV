88256f8b74d14bd012e34e496d8d0e38315dd494
Ray <851836818@qq.com>
2025-02-25 14:52:01 -0500

    Update download_install_env.sh

--- snake_pipeline/download_install_env.sh
+++ snake_pipeline/download_install_env.sh
@@ -1,5 +1,5 @@
 gdown https://drive.google.com/uc?id=1i3H6Z7VrYfwlPVPQojx8Ifb77HQST7aG
-gdown https://drive.google.com/uc?id=1Az0MKy0MrnFWhPG0U3o7znZnQidfPpB2
+gdown https://drive.google.com/uc?id=15sz0zputf3miLRAGlv5FeFJfaF7Gkh-p
 mkdir accusnv_env
 tar -xzf accusnv_env.tar.gz -C accusnv_env
 tar -xzf accusnv_sub.tar.gz
